191a74df15df73687220cfed85786eb3d2f11a79
ismael993-create <ismael.7@live.de>
2025-10-24 11:55:22 +0200

    Anpassung von style und script mit responsive box menu pene al salmone

--- penescript.js
+++ penescript.js
@@ -11,3 +11,22 @@ let liste = document.getElementById("zutatenListe");
    zutaten[i].getElementsByClassName("menge")[0].textContent = neueMenge;
   }
 }
+
+
+let box = document.getElementById('resp_menu');
+let btnOpen = document.getElementById('menuToggle');
+let btnClose = document.getElementById('menuClose');
+
+function openClosedRespmenu() {
+  if (box.classList.contains('closed_menu')) {
+    box.classList.remove('closed_menu');
+    box.setAttribute('aria-hidden', 'false');
+  } else {
+    box.classList.add('closed_menu');
+    box.setAttribute('aria-hidden', 'true');
+  }
+}
+
+
+btnOpen.onclick = openClosedRespmenu;
+btnClose.onclick = openClosedRespmenu;
