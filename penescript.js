2ed57176e4ddc0b96c104bbb92e384eeb7996c78
ismael993-create <ismael.7@live.de>
2025-10-26 12:09:19 +0100

    Dunkelmodus in pene rezept hinzugefügt

--- penescript.js
+++ penescript.js
@@ -30,3 +30,28 @@ function openClosedRespmenu() {
 
 btnOpen.onclick = openClosedRespmenu;
 btnClose.onclick = openClosedRespmenu;
+
+const themeSwitch = document.querySelector('#themeSwitch');
+const body = document.body;
+
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark');
+    themeSwitch.textContent = '🌞 Hell';
+}
+
+
+function toggleTheme() {
+
+    body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+    themeSwitch.textContent = '🌞 Hell';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    themeSwitch.textContent = '🌙 Dunkel';
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+themeSwitch.addEventListener('click', toggleTheme);
