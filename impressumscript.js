04c050dda2f409d8cdecfba995342ae824b702ed
Nick Ezenwa <nickezenwa38@gmail.com>
2025-10-27 15:59:38 +0100

    Nick Repzept optimiert

---
+++ impressumscript.js
@@ -0,0 +1,17 @@
+let box = document.getElementById('resp_menu');
+let btnOpen = document.getElementById('menuToggle');
+let btnClose = document.getElementById('menuClose');
+
+function openClosedRespmenu() {
+  if (box.classList.contains('closed_menu')) {
+    box.classList.remove('closed_menu');
+    box.setAttribute('aria-hidden', 'false');
+  } else {
+    box.classList.add('closed_menu');
+    box.setAttribute('aria-hidden', 'true');
+  }
+}
+
+
+btnOpen.onclick = openClosedRespmenu;
+btnClose.onclick = openClosedRespmenu;
