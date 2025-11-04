5fadec881757acda26dd4cd84da960545d9b4a47
Nick Ezenwa <nickezenwa38@gmail.com>
2025-11-04 20:44:58 +0100

    Meine neuen Änderungen

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-const themeSwitch = document.querySelector('#themeSwitch');
-const body = document.body;
+// const themeSwitch = document.querySelector('#themeSwitch');
+// const body = document.body;
 
-// damit die seite direkt dunkel lädt
-if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark');
-    themeSwitch.textContent = '🌞 Hell';
-}
+// // damit die seite direkt dunkel lädt
+// if (localStorage.getItem('theme') === 'dark') {
+//     body.classList.add('dark');
+//     themeSwitch.textContent = '🌞 Hell';
+// }
 
-//function zum wechsel zwischen hell und dunkel
-function toggleTheme() {
+// //function zum wechsel zwischen hell und dunkel
+// function toggleTheme() {
 
-    body.classList.toggle('dark');
+//     body.classList.toggle('dark');
 
-    if (body.classList.contains('dark')) {
-    themeSwitch.textContent = '🌞 Hell';
-    localStorage.setItem('theme', 'dark');
-  } else {
-    themeSwitch.textContent = '🌙 Dunkel';
-    localStorage.setItem('theme', 'light');
-  }
-}
+//     if (body.classList.contains('dark')) {
+//     themeSwitch.textContent = '🌞 Hell';
+//     localStorage.setItem('theme', 'dark');
+//   } else {
+//     themeSwitch.textContent = '🌙 Dunkel';
+//     localStorage.setItem('theme', 'light');
+//   }
+// }
 
-themeSwitch.addEventListener('click', toggleTheme);
+// themeSwitch.addEventListener('click', toggleTheme);
 
-let box = document.getElementById('resp_menu');
-let btnOpen = document.getElementById('menuToggle');
-let btnClose = document.getElementById('menuClose');
+// let box = document.getElementById('resp_menu');
+// let btnOpen = document.getElementById('menuToggle');
+// let btnClose = document.getElementById('menuClose');
 
-function openClosedRespmenu() {
-  if (box.classList.contains('closed_menu')) {
-    box.classList.remove('closed_menu');
-    box.setAttribute('aria-hidden', 'false');
-  } else {
-    box.classList.add('closed_menu');
-    box.setAttribute('aria-hidden', 'true');
-  }
-}
+// function openClosedRespmenu() {
+//   if (box.classList.contains('closed_menu')) {
+//     box.classList.remove('closed_menu');
+//     box.setAttribute('aria-hidden', 'false');
+//   } else {
+//     box.classList.add('closed_menu');
+//     box.setAttribute('aria-hidden', 'true');
+//   }
+// }
 
 
-btnOpen.onclick = openClosedRespmenu;
-btnClose.onclick = openClosedRespmenu;
+// btnOpen.onclick = openClosedRespmenu;
+// btnClose.onclick = openClosedRespmenu;
 
